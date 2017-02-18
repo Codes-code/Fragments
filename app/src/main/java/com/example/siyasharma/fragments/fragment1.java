855a6f13ca3b7f3193a74a7a574d7b311592ea9a
siya sharma <siya sharma>
2017-02-18 16:31:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/siyasharma/fragments/fragment1.java
@@ -0,0 +1,8 @@
+package com.example.siyasharma.fragments;
+
+/**
+ * Created by Siya Sharma on 18/02/2017.
+ */
+
+public class fragment1 {
+}
---
+++ app/src/main/java/com/example/siyasharma/fragments/fragments.java
@@ -0,0 +1,13 @@
+package com.example.siyasharma.fragments;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class fragments extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragments);
+    }
+}
